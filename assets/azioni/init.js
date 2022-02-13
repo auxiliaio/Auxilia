9a83ede8b3a814824d30cdd5fab210cb72e1e13c
Joshua M <josh@auxilia.io>
2022-02-13 14:38:13 +0300

    First upload

---
+++ assets/azioni/init.js
@@ -0,0 +1,49 @@
+'use strict'
+var testim = document.getElementById("testim");
+var testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children);
+var testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children);
+var testimSpeed = 15000;
+var currentSlide = 0;
+var currentActive = 0;
+var testimTimer;
+
+window.onload = function() {
+
+    // Testim Script
+    function playSlide(slide) {
+        for (var k = 0; k < testimDots.length; k++) {
+            testimContent[k].classList.remove("active");
+            testimContent[k].classList.remove("inactive");
+            testimDots[k].classList.remove("active");
+        }
+
+        if (slide < 0) {
+            slide = currentSlide = testimContent.length - 1;
+        }
+
+        if (slide > testimContent.length - 1) {
+            slide = currentSlide = 0;
+        }
+
+        if (currentActive != currentSlide) {
+            testimContent[currentActive].classList.add("inactive");
+        }
+        testimContent[slide].classList.add("active");
+        testimDots[slide].classList.add("active");
+
+        currentActive = currentSlide;
+
+        clearTimeout(testimTimer);
+        testimTimer = setTimeout(function() {
+            playSlide(currentSlide += 1);
+        }, testimSpeed)
+    }
+
+    for (var l = 0; l < testimDots.length; l++) {
+        testimDots[l].addEventListener("click", function() {
+            playSlide(currentSlide = testimDots.indexOf(this));
+        })
+    }
+
+    playSlide(currentSlide);
+}
