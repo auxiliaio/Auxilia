7ab2d524ca36805ab2029b68c4f8e0dde8367887
Joshua M <josh@auxilia.io>
2024-08-04 19:58:25 +0300

    Fixed loading bug

--- assets/azioni/init.js
+++ assets/azioni/init.js
@@ -95,18 +95,18 @@ window.onload = function() {
     }
 }
 
-// REVEAL ANIMATIONfunction reveal() {
-  var reveals = document.querySelectorAll(".svelare");
-
-  for (var i = 0; i < reveals.length; i++) {
-    var windowHeight = window.innerHeight;
-    var elementTop = reveals[i].getBoundingClientRect().top;
-    var elementVisible = 150;
-
-    if (elementTop < windowHeight - elementVisible) {
-      reveals[i].classList.add("active");
-    } else {
-      reveals[i].classList.remove("active");
-    }
-  }
-}
+// REVEAL ANIMATION// function reveal() {
+//   var reveals = document.querySelectorAll(".svelare");
+//
+//   for (var i = 0; i < reveals.length; i++) {
+//     var windowHeight = window.innerHeight;
+//     var elementTop = reveals[i].getBoundingClientRect().top;
+//     var elementVisible = 150;
+//
+//     if (elementTop < windowHeight - elementVisible) {
+//       reveals[i].classList.add("active");
+//     } else {
+//       reveals[i].classList.remove("active");
+//     }
+//   }
+// }
